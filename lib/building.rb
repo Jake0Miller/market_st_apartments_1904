eaa1d901b2186ae23d13d4084bb118f6679eda8f
Jake Miller <jake0miller@gmail.com>
2019-05-15 09:32:07 -0600

    Write iteration 3 and 4

--- lib/building.rb
+++ lib/building.rb
@@ -13,4 +13,16 @@ class Building
     total_rent = @units.inject(0) { |tot,unit| tot + unit.monthly_rent }
     total_rent.to_f / @units.length
   end
+
+  def renter_with_highest_rent
+    occupied_units = @units.find_all { |unit| !unit.renter.nil?}
+    occupied_units.max { |a,b| a.monthly_rent <=> b.monthly_rent }.renter
+  end
+
+  def annual_breakdown
+    @units.inject({}) do |hash,unit|
+      hash[unit.renter.name] = unit.monthly_rent*12 if !unit.renter.nil?
+      hash
+    end
+  end
 end
--- test/building_test.rb
+++ test/building_test.rb
@@ -3,10 +3,12 @@ require 'minitest/pride'
 require './lib/renter'
 require './lib/apartment'
 require './lib/building'
+require 'pry'
 
 class BuidlingTest < Minitest::Test
   def setup
     @jessie = Renter.new("Jessie")
+    @spencer = Renter.new("Spencer")
     @a1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
     @b2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
     @building = Building.new
@@ -30,5 +32,31 @@ class BuidlingTest < Minitest::Test
     @building.add_unit(@b2)
 
     assert_equal 1099.5, @building.average_rent
+  end
+
+  def test_highest_rent
+    @building.add_unit(@a1)
+    @building.add_unit(@b2)
+    @b2.add_renter(@spencer)
+
+    assert_equal @spencer, @building.renter_with_highest_rent
+
+    @a1.add_renter(@jessie)
+
+    assert_equal @jessie, @building.renter_with_highest_rent
+  end
+
+  def test_annual_breakdown
+    @building.add_unit(@a1)
+    @building.add_unit(@b2)
+    @b2.add_renter(@spencer)
+
+    expected = {"Spencer" => 11988}
+    assert_equal expected, @building.annual_breakdown
+
+    @a1.add_renter(@jessie)
+
+    expected = {"Jessie" => 14400, "Spencer" => 11988}
+    assert_equal expected, @building.annual_breakdown
   end
 end
